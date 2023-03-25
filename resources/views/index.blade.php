1d895e9d7b2dcfe8fc912c8900be24e9a315c997
Onen Sensy <sensyonen@gmail.com>
2023-03-25 11:54:54 +0300

    Lesson 8: Responsive Classes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,10 @@
         <div>
             <nav>
                 <div>
-                    <h1 class="font-bold uppercase p-4 border-b border-gray-100"><a href="/index"> Food Ninja</a>
+                    <h1 class="font-bold uppercase p-4 border-b border-gray-100">
+                        {{-- <a href="/index" class="text-green-500 sm:text-red-500 lg:text-blue-500 "> Food Ninja</a> --}}
+                        {{-- <a href="/index" class="text-sm mb:text-xl"> Food Ninja</a> --}}
+                        <a href="/index" class=""> Food Ninja</a>
                     </h1>
                 </div>
 
@@ -40,16 +43,16 @@
         </div>{{-- End nav --}}
 
         <main class="px-16 py-6">
-            <div class="flex justify-end">
+            <div class="flex justify-center md:justify-end">
                 <a href="#" class="text-primary">Login</a>
                 <a href="#" class="text-primary ml-2">Signup</a>
             </div>
 
-            <div class="flex items-end justify-center">
+            {{-- <div class="flex items-end justify-center">
                 <div class="bg-red-500 h-4 w-6"></div>
                 <div class="bg-blue-500 h-8 w-6"></div>
                 <div class="bg-green-500 h-12 w-6"></div>
-            </div>
+            </div> --}}
             <header>
                 <h2 class="text-gray-700 text-6xl font-semibold">Recipes</h2>
                 <h2 class="text-2xl font-semibold">For Ninjas</h2>
